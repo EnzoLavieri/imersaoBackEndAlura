9e814468f28dff56f6951c168a127a731b6b239f
EnzoLavieri <77125886+EnzoLavieri@users.noreply.github.com>
2024-11-18 15:49:40 -0300

    fix ignore node modules

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.listen(3000, () => {
   console.log("Servidor rodando no port 3000.");
 });
 
+//http://localhost:3000/api
+//Retorna um texto "Teste 1"
 app.get("/api", (req, res) => {
-  res.status(200).send();
+  res.status(200).send("Teste 1");
 });
