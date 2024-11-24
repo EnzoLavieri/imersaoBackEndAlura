76fa6a4529976cce747a8ddc6bef56b6aad013fc
EnzoLavieri <77125886+EnzoLavieri@users.noreply.github.com>
2024-11-24 15:43:36 -0300

    new function, add image path to db and fetch img logic in js

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { getTodosPosts, createNewPost } from "../models/postModel.js";
 
 export async function listarPosts(req, res) {
@@ -9,6 +10,24 @@ export async function postNewPost(req, res) {
   const newPost = req.body;
   try {
     const createdPost = await createNewPost(newPost);
+    res.status(200).json(createdPost);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({});
+  }
+}
+
+export async function uploadImg(req, res) {
+  const newPost = {
+    descricao: "",
+    imgUrl: req.file.originalname,
+    alt: "",
+  };
+
+  try {
+    const createdPost = await createNewPost(newPost);
+    const imgAtualized = `uploads/${createdPost.insertedId}.png`;
+    fs.renameSync(req.file.path, imgAtualized);
     res.status(200).json(createdPost);
   } catch (erro) {
     console.error(erro.message);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,28 @@
 import express from "express";
-import { listarPosts, postNewPost } from "../controllers/postController.js";
+import multer from "multer";
+import {
+  listarPosts,
+  postNewPost,
+  uploadImg,
+} from "../controllers/postController.js";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   app.use(express.json());
 
   app.get("/posts", listarPosts);
-
   app.post("/post", postNewPost);
+  app.post("/upload", upload.single("imagem"), uploadImg);
 };
 
 export default routes;
